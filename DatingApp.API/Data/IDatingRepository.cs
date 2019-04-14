2779ca288206c7d25d1d6a3757f0b1d0aa0c0f99
scaber <scaber1993@gmail.com>
2019-04-15 01:36:04 +0300

    likees

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -14,6 +14,7 @@ namespace DatingApp.API.Data
          Task<User> GetUser( int id );
          Task<Photo> GetPhoto( int id );
          Task<Photo> GetMainPhotoForUser(int userId);
+         Task<Like> GetLike(int userId, int recipientId);
 
      
     }
